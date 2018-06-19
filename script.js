48fa1427ec3ce9f6fe28ff455e10ebe1e8fe3a78
glickid <glickid@yahoo.com>
2018-06-19 22:16:02 +0300

    fix navbar issue

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function show_support_hide_header()
+{
+   var contact = document.getElementById("contact");
+   var header = document.getElementById("mainPage");
+
+   contact.classList.toggle('d-none');
+   header.classList.toggle('d-none');
+
+}
