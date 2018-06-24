8807ec2cdaf8c3fadc114047ce31b1ca5a72194c
glickid <glickid@yahoo.com>
2018-06-24 19:39:26 +0300

    Add selection before open a report

--- timeElapsed.js
+++ timeElapsed.js
@@ -18,6 +18,10 @@ function setDate() {
     document.getElementById("currentDate").innerHTML = d;
 
     time_spent();
+    getUrlVars();
+
+    var params = getUrlVars();
+    setDetails(params);
 }
 
 // THIS FUNCTION TAKES THE SECONDS ELAPSED AND CONVERTS THEM FOR OUTPUT
@@ -44,4 +48,70 @@ function time_spent() {
 // THIS FUNCTION INSERTS A LEADING ZERO (IF NECESSARY) TO PROVIDE UNIFORM OUTPUT
 function pad(num) {
     return ((num > 9) ? num : "0" + num);
+}
+
+function getUrlVars() {
+    // var vars = {};
+    // var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    //     vars[key] = value;
+    // });
+    var regex = /[?&]([^=#]+)=([^&#]*)/g,
+        url = window.location.href,
+        params = {},
+        match;
+    while (match = regex.exec(url)) {
+        params[match[1]] = match[2];
+    }
+    // alert(JSON.stringify(params))
+    return params;
+}
+
+function setDetails(params) {
+
+    var dr = params["dr-name"];
+    var man = params["optradio"];
+
+    if (dr === "1")
+    {
+        document.getElementById("aname").innerText="Dr. Who";
+    } 
+    else if (dr === "2")
+    {
+        document.getElementById("aname").innerText="Dr. Saab";
+    }
+    else if (dr === "3")
+    {
+        document.getElementById("aname").innerText="Dr. Mercedes";
+    }
+    else
+    {
+        document.getElementById("aname").innerText="Dr. Jones";
+    }
+    
+    
+    if (man === "1")
+    {
+        document.getElementById("cname").innerText="John Doe";
+        document.getElementById("snum").innerText="1234567890-18";
+        document.getElementById("stype").innerText="Complex";
+    }
+    else if (man === "2")
+    {
+        document.getElementById("cname").innerText="Button Benjamin";
+        document.getElementById("snum").innerText="0987654321-18";
+        document.getElementById("stype").innerText="Simple";
+    }
+    else //if (man === "3")
+    {
+        document.getElementById("cname").innerText="David Cohen";
+        document.getElementById("snum").innerText="12389045678-18";
+        document.getElementById("stype").innerText="Complex";
+    }
+    // else
+    // {
+    //     $("#cname").HTML="John Doe";
+    //     $("#snum").HTML="1234567890-18";
+    //     $("#stype").HTML="Complex";
+    // }
+    
 }
